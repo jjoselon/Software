8a29007ac435cedfb10be4636435bbb36e7f3b77
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2021-03-02 16:46:13 -0500

    Update extractAttributesNodeHTML.php

--- Symfony/Crawler/extractAttributesNodeHTML.php
+++ Symfony/Crawler/extractAttributesNodeHTML.php
@@ -4,6 +4,7 @@ function getNodeAttributes($html, $nodeFilter = "p", $attributes = array()) {
     $crawler = new Crawler();
     $crawler->addHtmlContent($html);
     $nodesAttributes = $crawler->filter($nodeFilter)->extract($attributes);
+    // @see https://api.drupal.org/api/drupal/vendor%21symfony%21dom-crawler%21Crawler.php/function/Crawler%3A%3Aextract/8.2.x
     foreach ($nodesAttributes as $nodeAttributes) {
         $data[] = array_combine($attributes, $nodeAttributes);
     }
